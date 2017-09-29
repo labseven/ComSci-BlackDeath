2a9945290719061728bafed21d9de766009d0764
Adam Novotny <adam.novotny@postrock.wall>
2017-09-29 10:04:34 -0400

    Extract import_owtrad to module for distribution

---
+++ code/make_graph.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import pandas as pd
+import numpy as np
+from import_owtrad import import_owtrad
+
+nodes, edges = import_owtrad()
+
+# print(nodes.info())
+# print()
+# print(edges.info())
