163a49a1036e001e4cfe6803a285be02c4851f18
Adam Novotny <adam.novotny@postrock.wall>
2017-10-07 10:36:25 -0400

    Update

--- code/make_graph.py
+++ code/make_graph.py
@@ -1,35 +1,60 @@
 import networkx as nx
 import pandas as pd
 import numpy as np
-from import_owtrad import import_owtrad
 import matplotlib.pyplot as plt
+
 import thinkplot
 import thinkstats2
 from thinkstats2 import Cdf, Pmf
 
-# new_csv = pd.read_csv('./OWTRAD-Files/OWTRAD-South German trade routes before 1500 CE-routes.csv'.strip(), header=0)
-# print(new_csv.iloc[10])
+from import_owtrad import import_owtrad
 
+
+# Import the OWTRAD dataset
 nodes, edges = import_owtrad()
+
+
+# Select the edges that we want to use
+edges_to_use = edges.loc[(edges.USES == 'trd') | (edges.USES == 'plg')]
+edges_list = zip(edges_to_use['NODE1'], edges_to_use['NODE2'])
 
 
 trade_edges = edges[edges['USES'] == 'trd']
 pilgrimadge_edges = edges[edges['USES'] == 'plg']
 
-print("Len trade routes:", len(trade_edges))
-print("Len pilgrimadeg routes:", len(pilgrimadge_edges))
 
-# print(pilgrimadge_edges)
 
-edges_list = zip(edges['NODE1'], edges['NODE2'])
-# print(next(edges_list))
 
+# build networks
+print("Trade routes:", len(trade_edges))
+print("Pilgrimadge routes:", len(pilgrimadge_edges))
+print()
+
+network_trade = nx.Graph()
+network_trade.add_edges_from(zip(trade_edges.NODEID1, trade_edges.NODEID2))
+
+network_pilgrimadge = nx.Graph()
+network_pilgrimadge.add_edges_from(zip(pilgrimadge_edges.NODEID1, pilgrimadge_edges.NODEID2))
+
+
+
+print("Trade nodes:", len(network_trade.nodes()))
+print("Pilgrimadge nodes:", len(network_pilgrimadge.nodes()))
+
+
+# Make nx graph
 G = nx.Graph()
 G.add_edges_from(edges_list)
 
-print("Len nodes:", len(G.nodes()))
-print("Len edges:", len(G.edges()))
-# print(G.nodes())
+
+print("G Len nodes:", len(G.nodes()))
+print("G Len edges:", len(G.edges()))
+
+node_df = pd.DataFrame(columns=["nodes", "susceptible", "infected", "dead"])
+node_df.nodes = G.nodes()
+node_df.susceptible = [100]*len(G.nodes())
+node_df.infected = [0]*len(G.nodes())
+node_df.dead = [0]*len(G.nodes())
 
 def draw_graph(G):
     nx.draw_circular(G,
@@ -37,62 +62,116 @@ def draw_graph(G):
                      with_labels=True)
     plt.show()
 
+# Visualize graph (SLOW on large graphs)
 # draw_graph(G)
 
 
+# infected_timestamps maps cities to a list of the timestamps at which they get infected (cities can be infected multiple times)
+infected_timestamps = dict.fromkeys(G.nodes(), frozenset())
+# cur_infected holds all the cities that are currently infected
+# It is seeded with the starting cities
+cur_infected = set(['Etil','Bukhara'])
 
-infected_times = dict.fromkeys(G.nodes(), frozenset())
-infected_set = set(['Etil','Bukhara'])
+
+def infect_city(node_df, node, infection_rate, mortality_rate):
+    node_df.set_value(node, "susceptible", 9)
+    node_df.get_value(node, 'susceptible')
+
+# Init i and infected_timestamps
 i = 0
-for city in infected_set:
-    infected_times[city] = infected_times[city].union(set([i]))
-
-print(infected_times["Etil"], infected_times['Srem'], infected_times['Trembowla'])
+for city in cur_infected:
+    infected_timestamps[city] = infected_timestamps[city].union(set([i]))
+    infect_city(node_df, city, 3, 0)
 
 
 def toss(p):
     return np.random.ranf() < p
 
-def step_infect(G, infected_set, p):
-    new_infected = set()
-    for infected_city in infected_set:
+
+# At each step, neighbors of cur_infected have a p chance of being infected
+def step_infect(G, cur_infected, p):
+    newly_infected = set()
+    for infected_city in cur_infected:
         neighbors = G[infected_city]
         for city in neighbors:
             if toss(p):
-                new_infected.add(city)
-    return new_infected
+                newly_infected.add(city)
+    return newly_infected
 
-total_infection = 0
+# Tracking the number of times the disease spreads
+infections_count = 0
+# Probability of disease transmission
 p = .1
 
-while (total_infection < 6 * len(G.nodes())): # 6 * len(G.nodes()
-    new_infected = step_infect(G, infected_set, p)
-    for city in new_infected:
-        infected_set.add(city)
-    total_infection += len(new_infected)
-    for city in new_infected:
-        infected_times[city] = infected_times[city].union(set([i]))
+
+# Run the simulation
+while (infections_count < 6 * len(G.nodes())):
+    newly_infected = step_infect(G, cur_infected, p)
+
+    for city in newly_infected:
+        cur_infected.add(city)
+    infections_count += len(newly_infected)
+
+    for city in newly_infected:
+        infected_timestamps[city] = infected_timestamps[city].union(set([i]))
 
     i+=1
 
-print(len(infected_set))
+
+# Analytics
+print(len(cur_infected))
 # print([(city, infected_times[city]) for city in infected_times if infected_times[city]])
 
-infection_count = dict()
-for city in infected_times:
-    infection_count[city] = len(infected_times[city])
+
+# Sum the number of times each city has been infected
+infections_per_city = dict()
+for city in infected_timestamps:
+    infections_per_city[city] = len(infected_timestamps[city])
+
 
 # infection_cdf = Cdf(infection_count.values())
 # thinkplot.Cdf(infection_cdf)
 # thinkplot.show()
 
+
+# Plotting degree distribution
 degree_of_node = dict()
 for node in G.nodes():
     degree_of_node[node] = len(G[node])
 
+
+def degree_pmf_graph(G):
+    degree_of_G_pmf = dict()
+    for node in G.nodes():
+        try:
+            degree_of_G_pmf[len(G[node])] += 1
+        except KeyError:
+            degree_of_G_pmf[len(G[node])] = 1
+    return degree_of_G_pmf
+
+degree_of_G_pmf = degree_pmf_graph(G)
+degree_of_trade_pmf = degree_pmf_graph(network_trade)
+degree_of_pilgrimadge_pmf = degree_pmf_graph(network_pilgrimadge)
+
+print("mean degree:", np.mean(list(degree_of_node.values())))
+
+# print(degree_of_G_pmf, degree_of_G_pmf.values())
+print(degree_of_trade_pmf)
+print(list(degree_of_trade_pmf.keys()))
+print(list(degree_of_trade_pmf.values()))
+
+
+plt.title('Our Degree Distribution')
+thinkplot.Plot(list(degree_of_G_pmf.keys()), list(degree_of_G_pmf.values()), label="Combined Network")
+thinkplot.Plot(list(degree_of_trade_pmf.keys()), list(degree_of_trade_pmf.values()), label="Trade Network")
+thinkplot.Plot(list(degree_of_pilgrimadge_pmf.keys()), list(degree_of_pilgrimadge_pmf.values()), label="Pilgrimadge Network")
+# thinkplot.show()
+
+
+plt.title('Degree Distribution Cdf')
 degree_cdf = Cdf(degree_of_node.values())
 thinkplot.Cdf(degree_cdf)
-thinkplot.show()
+# thinkplot.show()
 
 # for degree in range(degree_of_node.values().max(), 0, -1):
 
--- reports/notes.md
+++ reports/notes.md
@@ -9,3 +9,16 @@ SIS model: Cities never become uninfected
 "Relationship" means generalized least square
 
 We have 1155 nodes in trade routes (vs 1013 in research paper)
+
+
+10.2
+
+We have 2069 edges (vs 2084)
+We have 1570 nodes (vs 1311)
+
+
+10.3
+
+The paper says that Fig. S1 shows degree distribution. I do not think the plot looks reasonable. It is a log-log plot showing a flat line up until ~10 links. It is also calling it a "cummulative [sic] distribution," while the graph goes down. CDFs are not supposed to ever go down...
+
+ 
